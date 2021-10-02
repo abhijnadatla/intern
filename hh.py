353cf15a476c30ab2e76a38d51487ca62a6eed72
abhijnadatla <50200353+abhijnadatla@users.noreply.github.com>
2021-10-02 12:59:41 +0530

    Add files via upload

---
+++ hh.py
@@ -0,0 +1,7 @@
+import pandas as pd
+f = pd.read_csv ('input/question-2/main.csv')
+print(f.columns)
+f.head()
+fmin = df.groupby('occupation').age.agg(['min', 'max'])
+fmax
+fmax.to_csv('main.csv')
